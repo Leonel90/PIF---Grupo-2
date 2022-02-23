26c0e0779771a8c08d70bd07208d8fbb4d6282a9
Jinson25 <am987406@gmail.com>
2022-02-23 17:52:59 -0500

    main

--- js/index.js
+++ js/index.js
@@ -9,3 +9,30 @@ window.onscroll = () => {
         fondo.style.width = anchoFondo + "%"; //calcular el porcentaje decuador el tamaño del scrol de la pagina
     }
 }
+
+//galeria
+const imagenes = document.querySelectorAll('.imagenes')
+const imagenesAgrandar = document.querySelector('.img-agrandar')
+const contenedorImagen = document.querySelector('.imagen-grande')
+const menu = document.querySelector('.menu');
+
+imagenes.forEach(imagen =>{
+    imagen.addEventListener('click', ()=>{
+        aparecerImagen(imagen.getAttribute('src'))
+    })
+})
+contenedorImagen.addEventListener('click', (e)=>{
+    if(e.target !== imagenesAgrandar){
+        contenedorImagen.classList.toggle('show')
+        imagenesAgrandar.classList.toggle('showImagen')
+        menu.style.opacity = '1'
+    }
+})
+
+const aparecerImagen = (Imagen)=>{
+    imagenesAgrandar.src = Imagen;
+    contenedorImagen.classList.toggle('show')
+    imagenesAgrandar.classList.toggle('showImagen')
+    menu.style.opacity = '0'
+
+}
