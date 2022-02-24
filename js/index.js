f1cad423289d708fca0e7bb5500d5750a49cb5af
Jinson25 <am987406@gmail.com>
2022-02-24 10:14:41 -0500

    main

--- js/index.js
+++ js/index.js
@@ -1,15 +1,3 @@
-//variables para header
-const altura = document.body.scrollHeight - window.innerHeight; //accedemos al scrol del tamaño completo y restamos el alto
-const fondo = document.getElementById("fondo"); //accedemos al id de fondo 
-
-//calculo dque funcione cada vez que el usuario da scroll en la pagina 
-window.onscroll = () => {
-    const anchoFondo = (window.pageYOffset / altura) * 900; 
-    if(anchoFondo <= 100){
-        fondo.style.width = anchoFondo + "%"; //calcular el porcentaje decuador el tamaño del scrol de la pagina
-    }
-}
-
 //galeria
 const imagenes = document.querySelectorAll('.imagenes')
 const imagenesAgrandar = document.querySelector('.img-agrandar')
