b76a1237d835a140f24fa6ba774728d1b9c851fa
Jinson25 <am987406@gmail.com>
2022-02-25 14:29:21 -0500

    main

--- js/index.js
+++ js/index.js
@@ -30,3 +30,23 @@ const grid =  new Muuri('.grid',{
         rounding: false
     }
 });
+
+window.addEventListener('load', () => {
+    grid.refreshItems().layout();
+
+    const enlaces = document.querySelectorAll('#categorias a');
+    enlaces.forEach((elemento) => {
+        elemento.addEventListener('click', (evento) =>{
+            evento.preventDefault();
+            enlaces.forEach((enlace) => {enlace.classList.remove('activo')});
+            evento.target.classList.add('activo');
+
+            const categoria = evento.target.innerHTML.toLowerCase();
+            if (categoria === 'todo') {
+                grid.filter('[data-categoria]');
+            } else {
+                grid.filter(`[data-categoria="${categoria}"]`);
+            }
+        });
+    });
+})
