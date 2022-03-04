8647d300676d6bf17371c5ab94bc5b86b3d8710a
Leonel Sangolquiza <97122219+Leonel90@users.noreply.github.com>
2022-03-04 11:05:01 -0500

    login.js
    Cambios

---
+++ js/login.js
@@ -0,0 +1,16 @@
+function ir() {
+    alert("Bienveninos3");
+    window.location="inicio.html";
+    var Clave=1;
+    var Usuario="A";
+    /*if(document.form.txtusuario.value == Usuario && document.form.txtcontra.value==Clave){
+        alert("Bienveninos2");
+        window.location="inicio.html";
+    }
+    else{
+        alert(Usuario);
+        alert(Clave);
+    }*/
+
+    
+}
