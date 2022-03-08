84809ac945cea1030108f9163ba8de46afcdb339
Leonel90 <97122219+Leonel90@users.noreply.github.com>
2022-03-08 14:38:11 -0500

    html,css.js-->login
    implementación validación

--- js/login.js
+++ js/login.js
@@ -1,16 +1,100 @@
-function ir() {
-    alert("Bienveninos3");
-    window.location="inicio.html";
-    var Clave=1;
-    var Usuario="A";
-    /*if(document.form.txtusuario.value == Usuario && document.form.txtcontra.value==Clave){
-        alert("Bienveninos2");
-        window.location="inicio.html";
-    }
-    else{
-        alert(Usuario);
-        alert(Clave);
-    }*/
+document.getElementById("btn__iniciar-sesion").addEventListener("click", iniciarSesion);
+document.getElementById("btn__registrarse").addEventListener("click", register);
+window.addEventListener("resize", anchoPage);
 
-    
+//Declarando variables
+var formulario_login = document.querySelector(".formulario__login");
+var formulario_register = document.querySelector(".formulario__register");
+var contenedor_login_register = document.querySelector(".contenedor__login-register");
+var caja_trasera_login = document.querySelector(".caja__trasera-login");
+var caja_trasera_register = document.querySelector(".caja__trasera-register");
+
+//FUNCIONES
+
+function anchoPage() {
+
+    if (window.innerWidth > 850) {
+        caja_trasera_register.style.display = "block";
+        caja_trasera_login.style.display = "block";
+    } else {
+        caja_trasera_register.style.display = "block";
+        caja_trasera_register.style.opacity = "1";
+        caja_trasera_login.style.display = "none";
+        formulario_login.style.display = "block";
+        contenedor_login_register.style.left = "0px";
+        formulario_register.style.display = "none";
+    }
 }
+
+anchoPage();
+
+
+function iniciarSesion() {
+    if (window.innerWidth > 850) {
+        formulario_login.style.display = "block";
+        contenedor_login_register.style.left = "10px";
+        formulario_register.style.display = "none";
+        caja_trasera_register.style.opacity = "1";
+        caja_trasera_login.style.opacity = "0";
+    } else {
+        formulario_login.style.display = "block";
+        contenedor_login_register.style.left = "0px";
+        formulario_register.style.display = "none";
+        caja_trasera_register.style.display = "block";
+        caja_trasera_login.style.display = "none";
+    }
+}
+
+function register() {
+    if (window.innerWidth > 850) {
+        formulario_register.style.display = "block";
+        contenedor_login_register.style.left = "410px";
+        formulario_login.style.display = "none";
+        caja_trasera_register.style.opacity = "0";
+        caja_trasera_login.style.opacity = "1";
+    } else {
+        formulario_register.style.display = "block";
+        contenedor_login_register.style.left = "0px";
+        formulario_login.style.display = "none";
+        caja_trasera_register.style.display = "none";
+        caja_trasera_login.style.display = "block";
+        caja_trasera_login.style.opacity = "1";
+    }
+}
+// validacion y captura de datos del login mediante el formulario
+const nombre = document.getElementById("name")
+const email = document.getElementById("email")
+const usuario = document.getElementById("usuario")
+const password = document.getElementById("password")
+const div = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
+
+div.addEventListener("submit", e => {
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    parrafo.innerHTML = ""
+
+    if (nombre.value.length < 10) {
+        warnings += `El nombre ingresado no es válido <br/>`
+        entrar = true
+    }
+    if (!regexEmail.test(email.value)) {
+        warnings += `El email no es valido <br/>`
+        entrar = true
+    }
+
+    if (usuario.value.length < 6) {
+        warnings += `El nombre d usuario no es valido <br/>`
+        entrar = true
+    }
+
+    if (password.value.length < 8) {
+        warnings += `la contraseña no es valida <br/>`
+        entrar = true
+    }
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }
+})
