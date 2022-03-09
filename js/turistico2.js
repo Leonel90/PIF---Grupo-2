b7eacdc7c85e0ea0b838b080f15bf664d1fbb455
Jinson25 <am987406@gmail.com>
2022-03-08 19:51:46 -0500

    main

--- js/turistico2.js
+++ js/turistico2.js
@@ -1,158 +1,143 @@
-var app = new function() {
-    var vuelo1 = {
-      origen: "A",
-      destino: "V",
-      hora: new Date(2018, 4, 15, 12),
-      costobase: 500,
-      costoneto: 0,
-      reservas: []
-    };
-    var vuelo2 = {
-      origen: "B",
-      destino: "W",
-      hora: new Date(2018, 4, 28, 7, 30),
-      costobase: 450,
-      costoneto: 0,
-      reservas: []
-    };
-    var vuelo3 = {
-      origen: "C",
-      destino: "X",
-      hora: new Date(2018, 4, 21, 18),
-      costobase: 600,
-      costoneto: 0,
-      reservas: []
-    };
-    var vuelo4 = {
-      origen: "D",
-      destino: "Y",
-      hora: new Date(2018, 4, 28, 6, 50),
-      costobase: 550,
-      costoneto: 0,
-      reservas: [1078]
-    };
-    var vuelo5 = {
-      origen: "E",
-      destino: "Z",
-      hora: new Date(2018, 5, 1, 19, 30),
-      costobase: 200,
-      costoneto: 0,
-      reservas: []
-    };
-    this.vuelos = [vuelo1, vuelo2, vuelo3, vuelo4, vuelo5];
-    for (var i = 0; i < this.vuelos.length; i++) {
-      var aumentomanana = 0;
-      var aumentofindesemana = 0;
-      if (this.vuelos[i].hora.getHours() <= 12) {
-        aumentomanana = this.vuelos[i].costobase * 0.05; 
-      }
-      if (this.vuelos[i].hora.getDay() == 5 || this.vuelos[i].hora.getDay() == 6) {
-        aumentofindesemana = this.vuelos[i].costobase * 0.1; 
-      }
-      this.vuelos[i].costoneto = this.vuelos[i].costobase + aumentomanana + aumentofindesemana;
-    }
+var app = new function () {
+  var Ruta1 = {
+    origen: "Plaza del panecillo",
+    destino: "Monumento del panecillo",
+    hora: new Date(2022, 3, 09, 12),
+    costobase: 500 + " $",
+    reservas: []
+  };
+  var Ruta2 = {
+    origen: "B",
+    destino: "W",
+    hora: new Date(2022, 4, 28, 7, 30),
+    costobase: 450 + " $",
+    reservas: []
+  };
+  var Ruta3 = {
+    origen: "C",
+    destino: "X",
+    hora: new Date(2022, 4, 21, 18),
+    costobase: 600 + " $",
+    reservas: []
+  };
+  var Ruta4 = {
+    origen: "D",
+    destino: "Y",
+    hora: new Date(2022, 4, 28, 6, 50),
+    costobase: 550 + " $",
+    reservas: [1078]
+  };
+  var Ruta5 = {
+    origen: "E",
+    destino: "Z",
+    hora: new Date(2022, 5, 1, 19, 30),
+    costobase: 200 + " $",
+    reservas: []
+  };
+  //definir los objetos dentro de un array
+  this.rutas = [Ruta1, Ruta2, Ruta3, Ruta4, Ruta5];
 
-    this.mostrarVuelos = function() {
-      var data = '<br>';
-      if (this.vuelos.length > 0) {
-        for (i = 0; i < this.vuelos.length; i++) {
-          var hora = this.vuelos[i].hora.getHours() < 10 ? '0' + this.vuelos[i].hora.getHours() : this.vuelos[i].hora.getHours();
-          var minutos = this.vuelos[i].hora.getMinutes() < 10 ? '0' + this.vuelos[i].hora.getMinutes() : this.vuelos[i].hora.getMinutes();
+  //motrar todas las rutas disponibles con el array y obetos
+  this.mostrarrutas = function () {
+    var data = '<br>';
+    if (this.rutas.length > 0) {
+      for (i = 0; i < this.rutas.length; i++) {
+        var hora = this.rutas[i].hora.getHours() < 10 ? '0' + this.rutas[i].hora.getHours() : this.rutas[i].hora.getHours();
+        var minutos = this.rutas[i].hora.getMinutes() < 10 ? '0' + this.rutas[i].hora.getMinutes() : this.rutas[i].hora.getMinutes();
+        data += '<tr>';
+        data += '<td class = "contenedortr">Ruta # ' + (i + 1) + "<br>" + 'ORIGEN: ' + this.rutas[i].origen + "<br>" + 'DESTINO: ' + this.rutas[i].destino + "<br>" + 'SALIDA: ' + this.rutas[i].hora.toDateString() + " " + hora + ":" + minutos + "<br>" + "Costo Estimado: " + this.rutas[i].costobase + '</td>';
+        data += '<td ><button class="btn" onclick="app.Reservar(' + i + ')">Reservar</button></td>';
+        data += '</tr>';
+      }
+    }
+    document.getElementById('rutas').innerHTML = data;
+    document.getElementById('rutas').style.display = 'block';
+  };
+
+  this.Reservar = function (item) {
+    var el = document.getElementById('documento');
+    document.getElementById('documento').value = "";
+    document.getElementById('name').value = "";
+    document.getElementById('datosrutas').style.display = 'block';
+    document.getElementById('rutas').style.display = 'none';
+    document.getElementById('menu1').style.display = 'none';
+    document.getElementById('menu2').style.display = 'none';
+    document.getElementById('btnback').style.display = 'block';
+
+    var hora = this.rutas[item].hora.getHours() < 10 ? '0' + this.rutas[item].hora.getHours() : this.rutas[item].hora.getHours();
+    var minutos = this.rutas[item].hora.getMinutes() < 10 ? '0' + this.rutas[item].hora.getMinutes() : this.rutas[item].hora.getMinutes();
+    document.getElementById('datosrutas').innerHTML = "Ruta: # " + (item + 1) + ":<br>ORIGEN: " + this.rutas[item].origen + '<br>DESTINO: ' + this.rutas[item].destino + '<br>SALIDA: ' + this.rutas[item].hora.toDateString() + " " + hora + ":" + minutos + '<br>PRECIO BASE: $' + this.rutas[item].costobase;
+    document.getElementById('campodoc').style.display = 'block';
+    self = this;
+    document.getElementById('reserva-edit').onsubmit = function () {
+      var d = el.value * 1;
+      if (isNaN(d) || d == 0) {
+        window.alert("Ingrese un dato correcto");
+      } else {
+        var flag = false;
+        for (j = 0; j < self.rutas.length; j++) {
+          var auxDoc = self.rutas[j].reservas.indexOf(d)
+          if (auxDoc != -1) {
+            if (self.rutas[j].hora.getFullYear() == self.rutas[item].hora.getFullYear() &&
+              self.rutas[j].hora.getMonth() == self.rutas[item].hora.getMonth() &&
+              self.rutas[j].hora.getDate() == self.rutas[item].hora.getDate()) {
+              flag = true;
+              break;
+            }
+          }
+        }
+        if (flag) {
+          window.alert("Error: usted ya tiene reservado un Ruta para esta fecha");
+        } else {
+          self.rutas[item].reservas.push(d);
+          window.alert("Ruta reservado correctamente");
+          document.getElementById('menu1').style.display = 'block';
+          document.getElementById('menu2').style.display = 'block';
+          document.getElementById('datosrutas').style.display = 'none';
+          document.getElementById('campodoc').style.display = 'none';
+
+          document.getElementById('btnback').style.display = 'none';
+        }
+      }
+    }
+  };
+
+  this.consultarReserva = function () {
+    var el = document.getElementById('docConsulta');
+    var d = el.value * 1;
+    if (isNaN(d) || d == 0) {
+      window.alert("Ingrese un dato correcto");
+    } else {
+      var data = '<p class = "subtitulo">Ruta reservada del usuario numero cedula:</p> ' + d;
+      for (i = 0; i < this.rutas.length; i++) {
+        var auxDoc = this.rutas[i].reservas.indexOf(d)
+        if (auxDoc != -1) {
+          var hora = this.rutas[i].hora.getHours() < 10 ? '0' + this.rutas[i].hora.getHours() : this.rutas[i].hora.getHours();
+          var minutos = this.rutas[i].hora.getMinutes() < 10 ? '0' + this.rutas[i].hora.getMinutes() : this.rutas[i].hora.getMinutes();
           data += '<tr>';
-          data += '<td>Vuelo # '+ (i+1) + ' ORIGEN: ' + this.vuelos[i].origen + ', DESTINO: ' + this.vuelos[i].destino + ', SALIDA: ' + this.vuelos[i].hora.toDateString() + " " + hora + ":" + minutos + '</td>';
-          data += '<td><button onclick="app.Reservar(' + i + ')">Reservar</button></td>';
+          data += '<td class ="consulata">Ruta # ' + (i + 1) + "<br> ORIGEN: " + this.rutas[i].origen + '<br> DESTINO: ' + this.rutas[i].destino + '<br> SALIDA: ' + this.rutas[i].hora.toDateString() + " " + hora + ":" + minutos + '</td>';
           data += '</tr>';
         }
       }
-      document.getElementById('vuelos').innerHTML = data;
-      document.getElementById('vuelos').style.display = 'block';
-    };
-
-    this.Reservar = function (item) {
-      var el = document.getElementById('documento');
-      document.getElementById('documento').value = "";
-      document.getElementById('datosrutas').style.display = 'block';
-      document.getElementById('vuelos').style.display = 'none';
-      document.getElementById('menu1').style.display = 'none';
-      document.getElementById('menu2').style.display = 'none';
-      document.getElementById('btnback').style.display = 'block';
-
-      var impuesto = this.vuelos[item].costobase == this.vuelos[item].costoneto ? '' : 'Impuesto mañana y/o fin de semana'; 
-      var hora = this.vuelos[item].hora.getHours() < 10 ? '0' + this.vuelos[item].hora.getHours() : this.vuelos[item].hora.getHours();
-      var minutos = this.vuelos[item].hora.getMinutes() < 10 ? '0' + this.vuelos[item].hora.getMinutes() : this.vuelos[item].hora.getMinutes();
-
-      document.getElementById('datosrutas').innerHTML = "VUELO # " + (item + 1) + ":<br>ORIGEN: " + this.vuelos[item].origen + '<br>DESTINO: ' + this.vuelos[item].destino + '<br>SALIDA: ' + this.vuelos[item].hora.toDateString() + " " + hora + ":" + minutos + '<br>PRECIO BASE: $' + this.vuelos[item].costobase + '<br>PRECIO NETO: $' + this.vuelos[item].costoneto + " " + impuesto;
-      document.getElementById('campodoc').style.display = 'block';
-      self = this;
-      document.getElementById('reserva-edit').onsubmit = function() {
-        var d = el.value * 1;
-        if (isNaN(d) || d == 0) {
-          window.alert("Ingrese un dato correcto");
-        }else{
-          var flag = false;
-          for (j = 0; j < self.vuelos.length; j++) {
-            var auxDoc = self.vuelos[j].reservas.indexOf(d)
-            if (auxDoc != -1) {
-              if (self.vuelos[j].hora.getFullYear() == self.vuelos[item].hora.getFullYear() &&
-                self.vuelos[j].hora.getMonth() == self.vuelos[item].hora.getMonth() &&
-                self.vuelos[j].hora.getDate() == self.vuelos[item].hora.getDate()) {
-                flag = true;
-                break;
-              }
-            }
-          }
-          if (flag) {
-            window.alert("Error: usted ya tiene reservado un vuelo para esta fecha");
-          }else{
-            self.vuelos[item].reservas.push(d);
-            window.alert("Vuelo reservado correctamente");
-            document.getElementById('menu1').style.display = 'block';
-            document.getElementById('menu2').style.display = 'block';
-            document.getElementById('datosrutas').style.display = 'none';
-            document.getElementById('campodoc').style.display = 'none';
-
-            document.getElementById('btnback').style.display = 'none';
-          }
-        }
+      if (data == '<p class = "subtitulo">Ruta reservada del usuario numero cedula:</p> ' + d) {
+        window.alert("No existen rutas asociados a dicho documento");
+      } else {
+        document.getElementById('menu1').style.display = 'none';
+        document.getElementById('menu2').style.display = 'none';
+        document.getElementById('rutas').style.display = 'block';
+        document.getElementById('rutas').innerHTML = data;
+        document.getElementById('btnback').style.display = 'block';
       }
-    };
+    }
+  };
 
-    this.consultarReserva = function () {
-      var el = document.getElementById('docConsulta');
-      var d = el.value * 1;
-      if (isNaN(d) || d == 0) {
-          window.alert("Ingrese un dato correcto");
-      }else{
-        var data = '<br>VUELOS RESERVADOS DE ' + d;
-        for (i = 0; i < this.vuelos.length; i++) {
-          var auxDoc = this.vuelos[i].reservas.indexOf(d)
-          if (auxDoc != -1) {
-            var hora = this.vuelos[i].hora.getHours() < 10 ? '0' + this.vuelos[i].hora.getHours() : this.vuelos[i].hora.getHours();
-            var minutos = this.vuelos[i].hora.getMinutes() < 10 ? '0' + this.vuelos[i].hora.getMinutes() : this.vuelos[i].hora.getMinutes();
-            data += '<tr>';
-            data += '<td>Vuelo # '+ (i+1) + "= ORIGEN: " + this.vuelos[i].origen + ', DESTINO: ' + this.vuelos[i].destino + ', SALIDA: ' + this.vuelos[i].hora.toDateString() + " " + hora + ":" + minutos + '</td>';
-            data += '</tr>';
-          }
-        }
-        if (data == '<br>VUELOS RESERVADOS DE ' + d) {
-          window.alert("No existen vuelos asociados a dicho documento");
-        }else{
-          document.getElementById('menu1').style.display = 'none';
-          document.getElementById('menu2').style.display = 'none';
-          document.getElementById('vuelos').style.display = 'block';
-          document.getElementById('vuelos').innerHTML = data;
-          document.getElementById('btnback').style.display = 'block';
-        }
-      }
-    };
-
-    this.Volver = function (){
-      document.getElementById('datosrutas').style.display = 'none';
-      document.getElementById('campodoc').style.display = 'none';
-      document.getElementById('vuelos').style.display = 'none';
-      document.getElementById('btnback').style.display = 'none';
-      document.getElementById('menu1').style.display = 'block';
-      document.getElementById('menu2').style.display = 'block';
-      document.getElementById('docConsulta').value = "";
-    };
+  this.Volver = function () {
+    document.getElementById('datosrutas').style.display = 'none';
+    document.getElementById('campodoc').style.display = 'none';
+    document.getElementById('rutas').style.display = 'none';
+    document.getElementById('btnback').style.display = 'none';
+    document.getElementById('menu1').style.display = 'block';
+    document.getElementById('menu2').style.display = 'block';
+    document.getElementById('docConsulta').value = "";
+  };
 }
